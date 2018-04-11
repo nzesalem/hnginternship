2202a33f197ef1f97fa95bdf0b0e8e6896f91286
Salem Nzeukwu <sjimfender@gmail.com>
2018-04-11 11:13:50 +0100

    Initial commit. Initial design all set

---
+++ src/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>HNG Internship</title>
+	<link rel="stylesheet" href="./assets/css/style.css">
+</head>
+<body>
+	<div class="wrap">
+		<header>
+			<div class="container">
+				<div class="header-content">
+					<a href="" class="logo">HNG Internship</a>
+					
+					<div class="menu-container">
+						<ul class="menu">
+							<li>
+								<a href="https://twitter.com/hnginternship" target="_blank">About</a>
+							</li>
+							<li>
+								<a href="https://twitter.com/markessien/status/983623045628792832" target="_blank">Sponsors</a>
+							</li>
+						</ul>
+						<a href="https://bit.ly/2Jv2VNj" target="_blank" class="btn btn-outline">Join Now</a>
+					</div>
+				</div>
+			</div>
+		</header>
+		<main>
+			<div class="container">
+				<div class="hero-content">
+					<h1 class="title">3,000+ <strong>Developers</strong>, <strong>Designers</strong> And <strong>Enthusiasts</strong></h1>
+					<h3 class="sub-title">
+						Join over 3,000 Interns arround the world in learning new tech skills. <br />
+						It doesn’t matter whether you are a complete Beginner or a complete Badass
+					</h3>
+					<div class="start">Start Now: <?php echo date('h:i:s A'); ?></div>
+					<a href="https://bit.ly/2Jv2VNj" target="_blank" class="btn btn-start">Get Started</a>
+				</div>
+			</div>
+		</main>
+		<footer>
+			<div class="container">
+				<p>Made with <span class="love">&hearts;</span> by <a href="https://twitter.com/nzesalem" target="_blank">Nzesalem</a></p>
+			</div>
+		</footer>
+	</div>
+</body>
+</html>
